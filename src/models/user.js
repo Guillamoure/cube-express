7031709e9341af5e94b8fcc7db245d6562ab81b1
Jack Hilscher <jhilscheriv@gmail.com>
2020-06-10 11:12:08 -0400

    added mongoose connection with user model

---
+++ src/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  }
+})
+
+userSchema.pre('save', async function(next) {
+  const user = this
+
+  if (user.isModified('password')){
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+
+  next()
+})
+
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
