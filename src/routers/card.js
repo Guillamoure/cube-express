05a09f79d784ac001bedad2290b6a62a9ecf03fc
Jack Hilscher <jhilscheriv@gmail.com>
2020-10-16 23:28:11 -0400

    search mtg database or own database witha query

--- src/routers/card.js
+++ src/routers/card.js
@@ -7,19 +7,54 @@ let url = "https://api.magicthegathering.io/v1/cards"
 router.get('/cards', async (req, res) => {
   console.log("YOOOOO")
   try {
-    const dbCards = await Card.find({})
-    fetch(`${url}?name=nyx`)
-    .then(r => {
-      console.log("grabbed them! converting....")
-      return r.json()
-    })
-    .then(data => {
-      console.log("found them!")
-      res.send(data)
+		if (!req.query.name){
+			const dbCards = await Card.find({})
+			res.send(dbCards)
+			return
+		}
 
-    })
+    const dbCards = await Card.find({name: {$regex: req.query.name, $options: "i"}})
+		if (dbCards.length){
+			console.log(`Found ${dbCards.length} card, including ${dbCards[0].name}!`)
+			res.send(dbCards)
+		} else {
+			console.log(`Searching API for ${req.query.name}...`)
+			fetch(`${url}?name=${req.query.name}`)
+			.then(r => {
+				console.log("grabbed them! converting....")
+				return r.json()
+			})
+			.then(async data => {
+				console.log("found them!")
+				let cards = data.cards.map(async c => {
+
+					Card.findOne({name: c.name, manaCost: c.manaCost})
+						.then(async duplicateCard => {
+							if (duplicateCard){
+
+								console.log(`${c.name} is already in the database`)
+								return duplicateCard
+							} else {
+
+								const card = new Card(c)
+								card.cardType = c.type
+
+								console.log("Creating", card.name)
+
+								await card.save()
+								return card
+							}
+
+						})
+
+				})
+
+				res.send(cards)
+
+			})
+		}
   } catch (e){
-    res.status(500).send()
+    res.status(500).send(e)
   }
 })
 
