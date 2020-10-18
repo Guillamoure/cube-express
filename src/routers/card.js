5f1b94e3e1bb3b179e7a3f2bc70565ba654ea732
Jack Hilscher <jhilscheriv@gmail.com>
2020-10-17 21:00:38 -0400

    refactored get request to filter unique and multiverseid exclusive cards. needs to refactor, see comments

--- src/routers/card.js
+++ src/routers/card.js
@@ -14,6 +14,7 @@ router.get('/cards', async (req, res) => {
 		}
 
     const dbCards = await Card.find({name: {$regex: req.query.name, $options: "i"}})
+		// const dbCards = []
 		if (dbCards.length){
 			console.log(`Found ${dbCards.length} card, including ${dbCards[0].name}!`)
 			res.send(dbCards)
@@ -26,30 +27,75 @@ router.get('/cards', async (req, res) => {
 			})
 			.then(async data => {
 				console.log("found them!")
-				let cards = data.cards.map(async c => {
 
-					Card.findOne({name: c.name, manaCost: c.manaCost})
-						.then(async duplicateCard => {
-							if (duplicateCard){
+				let uniqueCardNames = []
+				let uniqueAndValidCards = data.cards.filter((el, i, array) => {
+					if (el.multiverseid){
+						if (!uniqueCardNames.includes(el.name)){
+							uniqueCardNames.push(el.name)
+							return true
+						} else {
+							return false
+						}
+					} else {
+						return false
+					}
+				})
+				// console.log(uniqueAndValidCards)
+				let cards = uniqueAndValidCards.map(c => {
+					const card = new Card(c)
+					card.cardType = c.type
+					return card
+				})
+				// current code creates a formatted card, and sends that data
+				// the below code will check for duplicates that already exist, then create a card
+				// the below code does not wait to return the searched card, it will return an empty object
+				// not sure how to link async/await to wait for database look up and/or save to send that data to the frontend
 
-								console.log(`${c.name} is already in the database`)
-								return duplicateCard
-							} else {
 
-								const card = new Card(c)
-								card.cardType = c.type
+				let saveCards = await uniqueAndValidCards.map(async c => {
+					let dbCard = {foo: "bar"}
+					Card.findOne({name: c.name, manaCost: c.manaCost}, async (err, existingCard) => {
+						if (existingCard){
+							console.log(`${c.name} is already in the database`)
+							dbCard = existingCard
+						} else {
+							const card = new Card(c)
+							card.cardType = c.type
 
-								console.log("Creating", card.name)
+							console.log("Creating", card.name)
 
-								await card.save()
-								return card
-							}
-
-						})
+							dbCard = card
+							await card.save()
+						}
+					})
+					console.log(dbCard)
+					return dbCard
 
 				})
 
 				res.send(cards)
+				// .then(async duplicateCard => {
+					// 	debugger
+					// 	if (duplicateCard){
+						//
+						// 		console.log(`${c.name} is already in the database`)
+						// 		cards.push(duplicateCard)
+						// 	} else {
+							//
+							// 		const card = new Card(c)
+							// 		card.cardType = c.type
+							//
+							// 		console.log("Creating", card.name)
+							//
+							// 		await card.save()
+							// 		cards.push(card)
+							// 	}
+							//
+							// })
+				// debugger
+				//
+				// res.send(cards)
 
 			})
 		}
